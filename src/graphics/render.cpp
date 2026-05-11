918d51ecdd8785f31c5fc3fd2c182d785301b18d
Rikas <s.azimkar1@newcastle.ac.uk>
2026-05-11 01:36:15 +0100

    negative z values are discarded now

--- src/graphics/render.cpp
+++ src/graphics/render.cpp
@@ -13,9 +13,11 @@ void Mesh(RenderTarget& target, const model::Mesh& mesh, const math::Transform& 
   for (int i = 0; i < mesh.triangles.size(); i += 3) {
  	  math::double3 a = VertexToScreen(mesh.vertices[mesh.triangles[i]].position, transform, camera, width, height);
  		math::double3 b = VertexToScreen(mesh.vertices[mesh.triangles[i + 1]].position, transform, camera, width, height);
- 		math::double3 c = VertexToScreen(mesh.vertices[mesh.triangles[i + 2]].position, transform, camera, width, height);
+  	math::double3 c = VertexToScreen(mesh.vertices[mesh.triangles[i + 2]].position, transform, camera, width, height);
 
- 		int triIndex = i / 3;
+    if (a.z <= 0 || b.z <= 0 || c.z <= 0) continue;
+
+    int triIndex = i / 3;
  		double r = std::fmod(triIndex * 0.6180339887, 1.0); // 1 / φ
     double g = std::fmod(triIndex * 0.3819660113, 1.0); // 1 − 1/φ
     double d = std::fmod(triIndex * 0.7071067811, 1.0); // 1 / √2
