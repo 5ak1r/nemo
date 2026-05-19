f3ef0df9b87fe95125adfdb549bd56f5925c9013
Rikas <s.azimkar1@newcastle.ac.uk>
2026-05-19 23:37:55 +0100

    actually fixed indent error this time

--- main.cpp
+++ main.cpp
@@ -6,29 +6,29 @@
 
 int main() {
   const int width = 1920;
-	const int height = 1080;
+  const int height = 1080;
 
-	const model::Mesh mesh = io::obj::Read("resources/suzanne.obj");
-	const model::Mesh mesh2 = io::obj::Read("resources/plane.obj");
-	const model::Mesh mesh3 = io::obj::Read("resources/cow.obj");
+  const model::Mesh mesh = io::obj::Read("resources/suzanne.obj");
+  const model::Mesh mesh2 = io::obj::Read("resources/plane.obj");
+  const model::Mesh mesh3 = io::obj::Read("resources/cow.obj");
 
-	graphics::window::InitGLFW();
+  graphics::window::InitGLFW();
 
-	graphics::window::Window window(width, height, "test");
-	window.MakeCurrent();
+  graphics::window::Window window(width, height, "test");
+  window.MakeCurrent();
 
-	graphics::window::InitGLAD();
-	glViewport(0, 0, window.getWidth(), window.getHeight());
+  graphics::window::InitGLAD();
+  glViewport(0, 0, window.getWidth(), window.getHeight());
 
-	math::Transform transform({0, conversions::ToRadians(180)}, {0.0, 0.0, 10.0});
-	math::Transform transform2({0, conversions::ToRadians(180)}, {1.0, 0.2, 3.0});
-	math::Transform transform3({0, conversions::ToRadians(180)}, {-2.0, 0.2, 2.0});
+  math::Transform transform({0, conversions::ToRadians(180)}, {0.0, 0.0, 10.0});
+  math::Transform transform2({0, conversions::ToRadians(180)}, {1.0, 0.2, 3.0});
+  math::Transform transform3({0, conversions::ToRadians(180)}, {-2.0, 0.2, 2.0});
 
-	graphics::scene::Scene scene;
-	scene.camera = graphics::Camera(conversions::ToRadians(60));
-	scene.components.push_back({mesh, transform});
-	scene.components.push_back({mesh2, transform2});
-	scene.components.push_back({mesh3, transform3});
+  graphics::scene::Scene scene;
+  scene.camera = graphics::Camera(conversions::ToRadians(60));
+  scene.components.push_back({mesh, transform});
+  scene.components.push_back({mesh2, transform2});
+  scene.components.push_back({mesh3, transform3});
 
-	graphics::render::MainLoop(window, scene);
+  graphics::render::MainLoop(window, scene);
 }
